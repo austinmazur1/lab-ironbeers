59be151884a7046b2b3152ab06e47666f5570cfb
Austin Mazur <mazuraustin1@gmail.com>
2023-03-25 14:38:55 +0100

    Iterations 1 and 2 done, Kinda

--- app.js
+++ app.js
@@ -11,15 +11,47 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('images'));
 
 // Register the location for handlebars partials here:
 
 // ...
 
-// Add the route handlers here:
+// Add the route handlers shere:s
 
+//home handler
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {imageUrl: '/images/beer.png'});
 });
 
+//beers page handler
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers('burger')
+  .then((beerArr) => {
+    const data = {
+      beers: beerArr
+    }
+    res.render('beers', data);
+    console.log("data");
+  })
+  .catch(err => console.log('error'));
+});
+
+//random-beer page handler
+app.get('/random-beer', (req, res) => {
+  res.render('random-beer')
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+// console.log(await punkAPI.getBeers('pizza'));
+
+// const thing = await punkAPI.getBeers('pizza');
+// console.log(thing);
+async function asyncCall() {
+  const thing = await punkAPI.getBeers({'abv_gt': 8});
+  console.log(thing);
+}
+
+asyncCall()
